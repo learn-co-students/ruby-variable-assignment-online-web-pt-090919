6441812b19526365228d1baf93f621fe6ee0016f
Brianna Jordan <bryvjordan9@gmail.com>
2019-09-10 21:55:23 +0000

    Done.

--- variable.rb
+++ variable.rb
@@ -1,7 +1,3 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-def get_variable_from_file(variable)
-  greeting = 'Hello World'
-  puts greeting
-end
-end
+greeting = 'Hello World'
